c63c4d70a563944b7d7f24a5415a789fd55afc21
roro <zavrazka6@mail.ru>
2019-04-15 10:58:55 +0300

    Default Changelist

---
+++ src/test/java/InsuranceTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class InsuranceTest {
+    @Test
+    public void testInsurance () {
+        System.setProperty ("webdriver.chrome.driver", "dev/chromedriver.exe");
+
+    }
+}
